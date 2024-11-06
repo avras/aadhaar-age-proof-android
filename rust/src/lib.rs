7647824e02937d7dc4927ab9113e12db4956ae9b
Saravanan Vijayakumaran <sarva.v@gmail.com>
2024-11-06 14:40:48 +0530

    Added function to generate pp (untested)

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,6 +1,39 @@
+use nova_aadhaar_qr::circuit::AadhaarAgeProofCircuit;
+use nova_snark::{
+    provider::{PallasEngine, VestaEngine},
+    traits::{circuit::TrivialCircuit, snark::RelaxedR1CSSNARKTrait, Engine},
+    PublicParams,
+};
+
 uniffi::setup_scaffolding!();
 
 #[uniffi::export]
 fn hello_world() -> String {
     "Hello from Rust on Android!".to_string()
+}
+
+type E1 = PallasEngine;
+type E2 = VestaEngine;
+type EE1 = nova_snark::provider::ipa_pc::EvaluationEngine<E1>;
+type EE2 = nova_snark::provider::ipa_pc::EvaluationEngine<E2>;
+type S1 = nova_snark::spartan::snark::RelaxedR1CSSNARK<E1, EE1>;
+type S2 = nova_snark::spartan::snark::RelaxedR1CSSNARK<E2, EE2>;
+type C1 = AadhaarAgeProofCircuit<<E1 as Engine>::Scalar>;
+type C2 = TrivialCircuit<<E2 as Engine>::Scalar>;
+
+#[uniffi::export]
+fn generate_public_parameters() -> Vec<u8> {
+    let circuit_primary: C1 = AadhaarAgeProofCircuit::default();
+    let circuit_secondary: C2 = TrivialCircuit::default();
+
+    let pp = PublicParams::<E1, E2, C1, C2>::setup(
+        &circuit_primary,
+        &circuit_secondary,
+        &*S1::ck_floor(),
+        &*S2::ck_floor(),
+    )
+    .unwrap();
+
+    let serialized_pp = bincode::serialize(&pp).unwrap();
+    serialized_pp
 }
