19a436494ba8c3d8ddfdaaba485ce70c9c6784f1
Saravanan Vijayakumaran <sarva.v@gmail.com>
2024-11-17 09:25:52 +0530

    Added QR image file chooser

--- kotlin/app/src/main/java/aadhaar/ageproof/ui/ProveScreen.kt
+++ kotlin/app/src/main/java/aadhaar/ageproof/ui/ProveScreen.kt
@@ -4,26 +4,41 @@ import aadhaar.ageproof.R
 import aadhaar.ageproof.data.AgeProofUiState
 import aadhaar.ageproof.data.QrCodeScanStatus
 import aadhaar.ageproof.ui.theme.AadhaarAgeProofTheme
+import android.graphics.BitmapFactory
+import android.net.Uri
 import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.google.zxing.BinaryBitmap
+import com.google.zxing.MultiFormatReader
+import com.google.zxing.NotFoundException
+import com.google.zxing.RGBLuminanceSource
+import com.google.zxing.common.HybridBinarizer
 import io.github.g00fy2.quickie.QRResult
 import io.github.g00fy2.quickie.ScanCustomCode
 import io.github.g00fy2.quickie.config.BarcodeFormat
 import io.github.g00fy2.quickie.config.ScannerConfig
+import java.io.FileNotFoundException
 
 
 @Composable
@@ -54,6 +69,40 @@ fun ProveScreen(
         }
     }
 
+    val context = LocalContext.current
+    val imageFileChooserLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.PickVisualMedia(),
+        onResult = { uri: Uri? ->
+            if (uri != null) {
+                try {
+                    val inputStream = context.contentResolver.openInputStream(uri)
+                    val bitmap = BitmapFactory.decodeStream(inputStream)
+                    val width = bitmap.width
+                    val height = bitmap.height
+                    val pixels = IntArray(width * height)
+                    bitmap.getPixels(pixels, 0, width, 0, 0, width, height)
+                    bitmap.recycle()
+                    val source = RGBLuminanceSource(width, height, pixels)
+                    val bBitmap = BinaryBitmap(HybridBinarizer(source))
+                    val reader = MultiFormatReader()
+                    try {
+                        val result = reader.decode(bBitmap)
+                        val resultText = result?.text ?: ""
+                        setQrCodeBytes(resultText)
+                    } catch (e: NotFoundException) {
+                        e.printStackTrace()
+                    }
+                } catch (e: FileNotFoundException) {
+                    e.printStackTrace()
+                } catch (e: NullPointerException) {
+                    e.printStackTrace()
+                }
+            } else {
+                setQrCodeBytes(String())
+            }
+        }
+    )
+
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier.fillMaxWidth(),
@@ -70,21 +119,39 @@ fun ProveScreen(
             }
         }
 
-        val scanButtonEnabled =
-            ageProofUiState.ppGenerated && !ageProofUiState.proofGenerationInProgress
-        Button(
-            onClick = {
-                resetQrCodeBytes()
-                resetProof()
-                scanQrCodeLauncher.launch(scannerConfig)
-            },
-            enabled = scanButtonEnabled,
-            modifier = modifier.fillMaxWidth()
-        ) {
-            if (ageProofUiState.qrCodeData.isEmpty()) {
-                Text("Scan Aadhaar QR code")
-            } else {
-                Text("Scan another Aadhaar QR code")
+        Row {
+            val scanButtonEnabled =
+                ageProofUiState.ppGenerated && !ageProofUiState.proofGenerationInProgress
+            Button(
+                onClick = {
+                    resetQrCodeBytes()
+                    resetProof()
+                    scanQrCodeLauncher.launch(scannerConfig)
+                },
+                enabled = scanButtonEnabled,
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_qrcode),
+                    contentDescription = "Image icon",
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text("Scan QR code")
+            }
+            Spacer(modifier = Modifier.width(16.dp))
+            Button(
+                onClick = {
+                    resetQrCodeBytes()
+                    resetProof()
+                    imageFileChooserLauncher.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+                },
+                enabled = scanButtonEnabled,
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_image),
+                    contentDescription = "Image icon",
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text("Choose image")
             }
         }
 
